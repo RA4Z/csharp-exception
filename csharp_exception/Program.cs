be8ef4bbdca6ba37d57da494de86ca0b6e34dcf8
RA4Z <rraz639@gmail.com>
2023-06-28 14:04:09 -0300

    Criando excessão caso o saldo não seja o suficiente para sacar

--- csharp_exception/Program.cs
+++ csharp_exception/Program.cs
@@ -1,10 +1,14 @@
 ﻿using csharp_exception.Titular;
 using csharp_exception.Contas;
-
+using csharp_exception;
 
 try
 {
-    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+    ContaCorrente conta1 = new ContaCorrente(278, "1234-X");
+    conta1.Sacar(50);
+    Console.WriteLine(conta1.GetSaldo());
+    conta1.Sacar(150);
+    Console.WriteLine(conta1.GetSaldo());
 }
 catch(ArgumentException ex)
 {
@@ -12,5 +16,9 @@ catch(ArgumentException ex)
     Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
     Console.WriteLine(ex.Message);
     
+}
+catch(SaldoInsuficienteException ex)
+{
+    Console.WriteLine($"Operação negada! Saldo Insuficiente!\n{ex.Message}");
 }
 
